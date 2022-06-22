7891b9396f9f6d289b8317b01311945870bfb4fb
Achristo11 <36556282+Achristo11@users.noreply.github.com>
2022-06-22 19:53:13 -0400

    Word generator

--- board.js
+++ board.js
@@ -4,22 +4,33 @@ window.onload = function () {
           'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
           't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-    var word;
+    const wordList = "testing";
+    const word = Array.from(wordList)
     var catagory;
+    
+    var board = document.getElementById('word');
+    var answer = document.createElement('ul');
+    
+    for (let i = 0; i < word.length; i++){
+        answer.setAttribute("id", "word");
+        
+
+
+    }
+
+
 
     for (let i = 0; i < alphabet.length; i++){
         let button = document.createElement('button')
         button.innerText = alphabet[i]
         button.id = [alphabet[i]]
         button.addEventListener('click', () => {
-            alert(button.id)
             button.disabled = true
         })    
         document.getElementById("letters").appendChild(button)
         
     }
 
-    
 
 
 
