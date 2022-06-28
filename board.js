da4d4f289199a5793fcc4167e5c34e0b68c13306
Achristo11 <36556282+Achristo11@users.noreply.github.com>
2022-06-28 18:11:31 -0400

    Reset logic

--- board.js
+++ board.js
@@ -1,19 +1,23 @@
-window.onload = function () {
-
+window.onload = () => {
     var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-          'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-          't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-    const wordList = "testing";
-    const word = Array.from(wordList)
-    
+    console.log("reintialized");
+    const wordList = ["testing", "potato"];
     const answerArray = document.querySelector('#answer');
+
+    if (Math.random() * 10 <= 5){
+        var word = Array.from(wordList[0])
+    }else{
+        var word = Array.from(wordList[1])
+    }
 
     for (let i = 0; i < word.length; i++){
         let letter = document.createElement('li')
         answerArray.appendChild(letter)
     }
-   
+
     for (let i = 0; i < alphabet.length; i++){
         let button = document.createElement('button')
         button.innerText = alphabet[i]
@@ -31,7 +35,6 @@ window.onload = function () {
 
     function checkLetter(guess) {
         for (let i = 0; i < word.length; i++) {
-            // console.log(answerArray.children, answerArray.children[i])
             if ( word[i] === guess.innerText) {
                 answerArray.children[i].textContent = guess.innerText;
             }
